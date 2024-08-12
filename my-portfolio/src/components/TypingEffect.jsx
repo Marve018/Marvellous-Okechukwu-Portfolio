5faaf67635656acefe5938c424e32abadbc64ca8
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-08-12 20:20:10 +0100

    chore: Add TypingEffect component for displaying typing effect

---
+++ my-portfolio/src/components/TypingEffect.jsx
@@ -0,0 +1,29 @@
+import Typewriter from 'typewriter-effect';
+
+{/* Javascript implementation to acheive typing effect */}
+const TypewriterComponent = () => {
+    return (
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .pauseFor(1000)
+              .typeString("I'm a Software Engineer")
+              .pauseFor(1000)
+              .deleteChars(17)
+              .typeString('Backend Engineer')
+              .pauseFor(1000)
+              .deleteChars(17)
+              .typeString('Web Developer')
+              .pauseFor(1000)
+              .deleteAll()
+              .start();
+          }}
+          options={{
+            loop: true,
+            cursor: '_',
+          }}
+        />
+      );
+};
+
+export default TypewriterComponent;
