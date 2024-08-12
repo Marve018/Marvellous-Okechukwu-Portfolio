58e7fff99b0f545ae27b1b28378063ffef52e87d
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-08-12 20:20:37 +0100

    feat: Add Skills component for displaying skills section

---
+++ my-portfolio/src/components/data/skills_data.jsx
@@ -0,0 +1,81 @@
+// Data for the skills section
+import C from "../../assets/C.svg";
+import Python from "../../assets/Python.svg";
+import Js from "../../assets/Js.svg";
+import postgresql from "../../assets/postgresql.svg";
+import Linux from "../../assets/Linux.svg";
+import Reactjs from "../../assets/Reactjs.svg";
+import nodejs from "../../assets/nodejs.svg";
+import Git from "../../assets/Git.svg";
+import Mongodb from "../../assets/Mongodb.jpeg";
+
+
+
+const skills_data = [
+  {
+    img: C,
+    dataTooltipId: "C",
+    dataTooltipContent: "C Programming",
+    backgroungColor: "#042F52",
+    color: "white",
+  },
+  {
+    img: Reactjs,
+    dataTooltipId: "React",
+    dataTooltipContent: "React JS and React Native",
+    backgroungColor: "#00D8FF",
+    color: "white",
+  },
+  {
+    img: Js,
+    dataTooltipId: "Js",
+    dataTooltipContent: "JavaScript",
+    backgroungColor: "#444444",
+    color: "white",
+  },
+  {
+    img: Python,
+    dataTooltipId: "Python",
+    dataTooltipContent: "Python",
+    backgroungColor: "#F2BB30",
+    color: "#35668F",
+  },
+  {
+    img: postgresql,
+    dataTooltipId: "PostgreSQL",
+    dataTooltipContent: "PostgreSQL",
+    backgroungColor: "#1A5F8A",
+    color: "white",
+  },
+  {
+    img: nodejs,
+    dataTooltipId: "NodeJS",
+    dataTooltipContent: "Node JS",
+    backgroungColor: "#333333",
+    color: "#58A846",
+  },
+  {
+    img: Mongodb,
+    dataTooltipId: "MongoDB",
+    dataTooltipContent: "Mongo DB",
+    backgroungColor: "#3F9D3C",
+    color: "#49484A",
+  },
+  {
+    img: Git,
+    dataTooltipId: "git",
+    dataTooltipContent: "Git and GitHub",
+    backgroungColor: "black",
+    color: "#F34F29",
+  },
+  {
+    img: Linux,
+    dataTooltipId: "Linux",
+    dataTooltipContent: "Linux",
+    backgroungColor: "black",
+    color: "white",
+  },
+  // Add more skills data...
+];
+
+export default skills_data;
